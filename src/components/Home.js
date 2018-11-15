82736cfd7140282a0d2605de412e8ca2ba20f512
hgx01 <117622536@qq.com>
2018-11-15 13:39:25 +0800

    react+scss starter

---
+++ src/components/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './home.scss';
+
+class Home extends Component {
+  render() {
+    return (
+      <div className="home">
+        
+        这是Home组件
+      </div>
+    );
+  }
+}
+
+export default Home;
