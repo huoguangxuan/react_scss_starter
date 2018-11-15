fea1982fdd31a85a8f2ccb3190d2cca94eac450a
hgx02 <18618110533@163.com>
2018-11-15 15:04:12 +0800

    a little modify

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react';
-import './App.css';
-import Home from './Home.js';
+import './App.scss';
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Home/>
+        aa1111
       </div>
     );
   }
